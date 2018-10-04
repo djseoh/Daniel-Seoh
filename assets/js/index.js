29a8abc1cc29b403fe21b0e59e5491c50325f1e8
daniel seoh <djseoh@gmail.com>
2018-10-04 16:00:50 -0400

    added screen shots

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // Transition effect for navbar 
     $(window).scroll(function() {
       // checks if window is scrolled more than 500px, adds/removes solid class
-      if($(this).scrollTop() > 630) { 
+      if($(this).scrollTop() > 500) { 
           $('.nav').addClass('solid');
       } else {
           $('.nav').removeClass('solid');
